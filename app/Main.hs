2114406e414e869a6d3aa13022e77d277700b2b6
Tom Johnson <public@tomjohnson.me.uk>
2018-05-01 21:53:42 +0100

    Log the output of the child

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,31 @@ module Main where
 
 import qualified Screen
 import qualified Data.ByteString as BS
-import qualified Data.Conduit.Combinators as C (stdout, stdin)
+import qualified Data.Conduit.Combinators as C
 import Control.Concurrent (forkIO, MVar(..), newEmptyMVar, putMVar, takeMVar)
 import Control.Monad (void)
-import Data.Conduit ((.|), runConduit)
+import Data.ByteString (ByteString(..), hPut)
+import Data.Conduit (ConduitT(..), (.|), runConduit, bracketP, awaitForever, yieldM)
 import System.Exit (exitSuccess)
-import System.IO (BufferMode(..), hSetBuffering, hFlush, stdin, stdout)
-import System.Posix.Signals (
-    installHandler,
-    Signal(..),
-    Handler(..),
-    keyboardSignal,
-    keyboardTermination,
-    softwareTermination)
+import System.IO
+    ( BufferMode(..)
+    , IOMode(..)
+    , hSetBuffering
+    , hFlush
+    , hSetEcho
+    , stdin
+    , stdout
+    , hClose
+    , openFile
+    )
+import System.Posix.Signals
+    ( installHandler
+    , Signal(..)
+    , Handler(..)
+    , keyboardSignal
+    , keyboardTermination
+    , softwareTermination
+    )
 import System.Process (CmdSpec(..))
 
 setup :: IO (MVar ())
@@ -29,7 +41,21 @@ setup = do
     termVar <- newEmptyMVar
     let install sig = installHandler sig (Catch $ putMVar termVar ()) Nothing
     install `mapM_` killSigs
+    hSetEcho stdin False
     return termVar
+
+handleOutput :: Screen.Screen -> IO ()
+handleOutput screen = do
+    log <- openFile "log" WriteMode
+    hSetBuffering log NoBuffering
+    runConduit $ Screen.outputConduit screen
+        .| logger log
+        .| C.stdout
+    hClose log
+  where
+    logger handle = awaitForever $ \ str -> yieldM $ do
+        hPut handle str
+        return str
 
 main :: IO ()
 main = do
@@ -37,9 +63,11 @@ main = do
     hSetBuffering stdin NoBuffering
     hSetBuffering stdout NoBuffering
     screen <- Screen.new (RawCommand "/usr/bin/bash" [])
-    void . forkIO . runConduit $ Screen.outputConduit screen .| C.stdout
     void . forkIO . runConduit $ C.stdin .| Screen.inputConduit screen
+    void . forkIO . handleOutput $ screen
     void . takeMVar $ doExit
     putStrLn ""
     putStrLn "Exiting"
+    hSetEcho stdin True
     hFlush stdout
+    exitSuccess
