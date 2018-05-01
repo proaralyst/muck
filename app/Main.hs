c886feecc19c11149c8532a7053c3c0e454d1dd1
Tom Johnson <public@tomjohnson.me.uk>
2018-05-01 21:08:17 +0100

    Capture exit

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,46 @@ module Main where
 import qualified Screen
 import qualified Data.ByteString as BS
 import qualified Data.Conduit.Combinators as C (stdout, stdin)
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, MVar(..), newEmptyMVar, putMVar, takeMVar)
+import Control.Monad (void)
 import Data.Conduit ((.|), runConduit)
-import System.IO (BufferMode(..), hSetBuffering, stdin, stdout)
+import System.Exit (exitSuccess)
+import System.IO (BufferMode(..), hSetBuffering, hFlush, stdin, stdout)
+import System.Posix.Signals (
+    installHandler,
+    Signal(..),
+    Handler(..),
+    keyboardSignal,
+    keyboardTermination,
+    softwareTermination)
 import System.Process (CmdSpec(..))
+
+tearDown :: MVar () -> IO ()
+tearDown mvar = putMVar mvar ()
+
+setup :: IO (MVar ())
+setup = do
+    hSetBuffering stdin NoBuffering
+    hSetBuffering stdout NoBuffering
+    let killSigs =
+            [ keyboardSignal
+            , keyboardTermination
+            , softwareTermination
+            ]
+    termVar <- newEmptyMVar
+    let install sig = installHandler sig (Catch . tearDown $ termVar) Nothing
+    install `mapM_` killSigs
+    return termVar
 
 main :: IO ()
 main = do
+    doExit <- setup
     hSetBuffering stdin NoBuffering
     hSetBuffering stdout NoBuffering
     screen <- Screen.new (RawCommand "/usr/bin/bash" [])
-    _ <- forkIO . runConduit $ Screen.outputConduit screen .| C.stdout
-    runConduit $ C.stdin .| Screen.inputConduit screen
+    void . forkIO . runConduit $ Screen.outputConduit screen .| C.stdout
+    void . forkIO . runConduit $ C.stdin .| Screen.inputConduit screen
+    void . takeMVar $ doExit
+    putStrLn ""
+    putStrLn "Exiting"
+    hFlush stdout
